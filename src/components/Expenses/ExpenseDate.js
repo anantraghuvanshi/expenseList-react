1988fa5ee95ee3186dec50351ab5fa15f87505ad
Anant Singh Raghuvanshi <43936308+anantraghuvanshi@users.noreply.github.com>
2022-02-28 14:12:28 +0530

    Initial files added

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css';
+function ExpenseDate(props){
+    const month = props.date.toLocaleString('en-US',{month: 'long'});
+    const day = props.date.toLocaleString('en-US',{day: '2-digit'});
+    const year = props.date.getFullYear();
+    return (<div classname = 'expense-date'>
+        <div classname = 'expense-date__month'>{month}</div>
+        <div classname = 'expense-year'>{year}</div>
+        <div classname = 'expense-date__day'>{day}</div>
+    </div>)
+}
+
+export default ExpenseDate;
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
+
+  return (
+      <li>
+    <Card className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+    </li>
+  );
+};
+
+export default ExpensesList;
